66b78b51463a8c46e857fe532cd52eba21969480
Djoann <djoann.fal@gmail.com>
2013-04-20 16:15:45 +0200

    First commit NASA Space app - economic impact - Paris

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+(function(){
+
+$(document).ready(function () {
+        //alert("Hello");
+        $("#navigation a").click(function () {
+            var href = $(this).attr("href"); 
+            alert(href) 
+        })
+})
+
+})();
